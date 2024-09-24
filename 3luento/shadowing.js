b7be1be60fb36285e6df1c0fd828c05667b39ac2
Maarit Välisuo <mvalisuo@ad1-83.ad.uwasa.fi>
2024-09-24 14:21:49 +0300

    Lisätty esimerkki muuttujan varjostamisesta.

---
+++ 3luento/shadowing.js
@@ -0,0 +1,22 @@
+
+/*
+  names on määritelty ensin päätasolla ja sitten varjostettu 
+  for-lohkon sisällä rivillä 16. Viimeisellä rivillä sen arvo
+  on "Alkuperäinen arvo"
+*/
+
+let names = ['Matti', 'Minna', 'Iines', 'Joni', 'Mikko', 'Leena']
+
+let name = "Alkuperäinen arvo"
+
+for (let i = 0; i < names.length; i++) {
+  let name = names[i]
+  console.log(name)
+}
+//Name-muuttujan arvo on sama kuin viimeksi TÄSSÄ lohkossa sijoitettu arvo.
+//for-silmukka on omassa lohkossaan.
+console.log(`Nimet loppuivat. Name muuttujan arvo nyt ${name} ja i oli viimeksi ${i}`)
+
+
+
+
