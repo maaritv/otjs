965769fed01e666c113406051733a6ae60c2ab2c
Sovellus Kehittäjä <test_programmer@MacBookAir.lan>
2025-10-01 12:18:31 +0300

    Parannettu refaktorotua koodia. erotettu esiehtotarkastus validoinnista.

--- 3luento/writefile_refactored.js
+++ 3luento/writefile_refactored.js
@@ -3,19 +3,29 @@ const path = require('path');  //Hakemistojen yhdistäminen
 const readline = require('readline-sync'); //syötteen lukeminen
 
 function validateFilePath(filePath) {
+    if (filePath.length===0) {
+        throw new Error('Directory name must not be empty!');
+    }
+}
+
+function checkFilePath(filePath) {
     if (filePath.startsWith('/')) {
-        throw new Error('Writing to the root is not allowed!');
+        throw new Error('Writing to the root is not allowed in this app!');
     }
 }
 
 function validateData(data) {
-    if (data.length > 10) {
-        throw new Error('Data should be less than 10 characters long.');
+    if (data.length==0) {
+        throw new Error('Data must not be empty.');
+    }
+    if (data.length > 20) {
+        throw new Error('Data should be less than 20 characters long.');
     }
 }
 
 function writeDataToFile(dir, filename, data) {
     try {
+        checkFilePath(dir)
         // Yhdistetään tiedostopolku
         const filePath = path.join(dir, filename);
 
@@ -35,22 +45,18 @@ function getCurrentWorkingDirectory() {
 function main() {
     try {
         //Käyttöliittymätaso
-        const workingDir=getCurrentWorkingDirectory();
+        const workingDir = getCurrentWorkingDirectory();
         console.log(`You are now at: ${workingDir}`)
         const filePath = readline.question(`Enter the file path: `);
         validateFilePath(filePath)
 
         let data = readline.question('Enter the data to write to the file (max 10 characters): ');
         validateData(data)
-        //Tässä sovelluksessa nimeä ei kysytä käyttäjältä, voitaisi kyllä
+        //Tässä sovelluksessa nimeä ei kysytä käyttäjältä, voitaisi kyllä. ei tarvitse validoida.
         const appFileName = 'appfile.txt';
-        try {
-            //Kutsutaan hyötyfunktiota tai palvelufunktiota.
-            writeDataToFile(filePath, appFileName, data)
-            console.log("No exceptions got, good, writing file succeeded.")
-        } catch (err) {
-            throw new Error('Failed to open or create the file: ' + err.message);
-        }
+        //Kutsutaan hyötyfunktiota tai palvelufunktiota.
+        writeDataToFile(filePath, appFileName, data)
+        console.log("No exceptions got, good, writing file succeeded.")
     } catch (err) {
         console.error('Error:', err.message);
     }
